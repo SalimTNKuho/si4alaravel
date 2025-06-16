33cde4cf4b01be8401c1300f2f5e4d2f8f95cf8f
SalimTNKuho <salimalfaruq_2327240061@mhs.mdp.ac.id>
2025-06-16 13:20:06 +0700

    route copy

--- routes/web copy.php
+++ routes/web copy.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\ProdiController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MateriController;
+use App\Http\Controllers\NilaiController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +20,10 @@ Route::get('/profil', function () {
 Route::resource('/fakultas', FakultasController::class);
 Route::resource('/prodi', ProdiController::class);
 Route::resource('/mahasiswa', MahasiswaController::class);
+Route::resource('/materi', MateriController::class);
+Route::resource('/nilai', NilaiController::class);
+Route::resource('/profile', ProfileController::class);
+Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
 Route::get('/dashboard', [DashboardController::class, 'index']);
 
 
