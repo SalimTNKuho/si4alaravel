a48ce00014093dffd4b7212ae7ae724708b0558c
SalimTNKuho <salimalfaruq_2327240061@mhs.mdp.ac.id>
2025-06-20 14:16:38 +0700

    pls work

--- resources/views/materi/index.blade.php
+++ resources/views/materi/index.blade.php
@@ -2,56 +2,45 @@
 @section('title', 'Materi')
 
 @section('content')
-<div class="container-fluid">
     <div class="row">
         <div class="col-12">
-            <div class="card card-primary">
+            <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">Materi</h3>
+                    <h3 class="card-title">List Materi</h3>
                     <div class="card-tools">
-                        <a href="{{ route('materi.create') }}" class="btn btn-tool btn-primary">
-                            <i class="fas fa-plus"></i> Tambah Materi
-                        </a>
+                        <a href="{{ route('materi.create') }}" class="btn btn-primary">Tambah</a>
                     </div>
                 </div>
                 <div class="card-body">
-                    <table class="table table-bordered table-hover">
-                        <thead class="table-primary">
-                            <tr>
-                                <th style="width: 10px">#</th>
-                                <th>Judul</th>
-                                <th>Deskripsi</th>
-                                <th style="width: 150px">Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($materi as $item)
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped">
+                            <thead>
                                 <tr>
-                                    <td>{{ $item->judul }}</td>
-                                    <td>{{ $item->deskripsi }}</td>
-                                    <td>
-                                        <a href="{{ route('materi.edit', $item->id) }}" class="btn btn-warning btn-sm">
-                                            <i class="fas fa-edit"></i> Edit
-                                        </a>
-                                        <form action="{{ route('materi.destroy', $item->id) }}" method="POST" style="display:inline;">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Yakin ingin menghapus?')">
-                                                <i class="fas fa-trash"></i> Hapus
-                                            </button>
-                                        </form>
-                                    </td>
+                                    <th>Judul</th>
+                                    <th>Deskripsi</th>
+                                    <th>Aksi</th>
                                 </tr>
-                            @empty
-                                <tr>
-                                    <td colspan="4" class="text-center">Data tidak tersedia</td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($materi as $item)
+                                    <tr>
+                                        <td>{{ $item->judul }}</td>
+                                        <td>{{ $item->deskripsi }}</td>
+                                        <td>
+                                            <a href="{{ route('materi.edit', $item->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                                            <form action="{{ route('materi.destroy', $item->id) }}" method="POST" class="d-inline">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Yakin ingin menghapus?')">Hapus</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
