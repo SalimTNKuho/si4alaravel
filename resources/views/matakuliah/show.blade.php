f2b769e529969fed565ce3e27837204a8c00d319
SalimTNKuho <salimalfaruq_2327240061@mhs.mdp.ac.id>
2025-06-26 13:00:27 +0700

    last one cmon

---
+++ resources/views/matakuliah/show.blade.php
@@ -0,0 +1,52 @@
+@extends('layout.main') {{-- Assuming AdminLTE 4 layout is named 'main' --}}
+@section('title', 'Matakuliah')
+
+@section('content')
+    <!--begin::Row-->
+    <div class="row">
+        <div class="col-12">
+        <!-- Default box -->
+        <div class="card">
+            <div class="card-header">
+            <h3 class="card-title">List Matakuliah</h3>
+            <div class="card-tools">
+                <button
+                type="button"
+                class="btn btn-tool"
+                data-lte-toggle="card-collapse"
+                title="Collapse">
+                <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
+                <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
+                </button>
+                <button
+                type="button"
+                class="btn btn-tool"
+                data-lte-toggle="card-remove"
+                title="Remove">
+                <i class="bi bi-x-lg"></i>
+                </button>
+                </div>
+            </div>
+            <!--end::Body-->
+            <!--begin::Footer-->
+            <div class="card-body">
+                <table class="table">
+                    <tr>
+                        <th>Kode MK</th>
+                        <td>{{ $matakuliah->kode_mk }}</td>
+                    </tr>
+                    <tr>
+                        <th>Nama</th>
+                        <td>{{ $matakuliah->nama }}</td>
+                    </tr>
+                        <th>Program Studi</th>
+                        <td>{{ $matakuliah->prodi->nama }}</td>
+                    </tr>
+                    </table>
+            </div>
+            <!--end::Footer-->
+        </div>
+        <!-- end::box -->
+    </div>
+    <!-- end::Row -->
+@endsection
